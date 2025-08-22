4936cd393440dee85499bc863f1eb5eb7223703d
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2025-08-22 13:07:49 -0300

    Finalizado el responsive dle home

--- frontend/src/components/Home/Search/Search.tsx
+++ frontend/src/components/Home/Search/Search.tsx
@@ -6,7 +6,7 @@ export default function Search() {
   //console.log(isValueSearch);
   return (
     <div className="flex flex-col w-full max-w-[740px] gap-y-3">
-      <article className="flex items-center gap-x-4 w-full">
+      <article className="flex flex-col sm:flex-row items-center gap-4 w-full">
         <input
           type="search"
           className="bg-[#B3B3B3]/50 px-4 py-4 rounded-lg w-full"
@@ -18,13 +18,13 @@ export default function Search() {
         />
         <a
           href={`/locals?s=${isValueSearch}`}
-          className="px-8 h-full bg-red-500 rounded-lg text-xl font-semibold cursor-pointer flex items-center justify-center"
+          className="px-8 py-4 h-full w-full sm:w-fit bg-red-500 rounded-lg text-base sm:text-xl font-semibold cursor-pointer flex items-center justify-center"
         >
           Buscar
         </a>
       </article>
-      <button className="px-8 py-4 h-full bg-red-500 rounded-lg text-xl font-semibold cursor-pointer flex items-center justify-center gap-x-2">
-        <BiSolidMap className="text-2xl" /> Ver locales en mapa
+      <button className="px-8 py-4 h-full bg-red-500 rounded-lg text-base sm:text-xl font-semibold cursor-pointer flex items-center justify-center gap-x-2">
+        <BiSolidMap className="text-xl sm:text-2xl" /> Ver locales en mapa
       </button>
     </div>
   );
